8a5dbd2f05e09c4219b818c9a94cb1ee16d26755
NonymousMorlock <agamspaul@gmail.com>
2024-06-02 06:56:09 +0000

    🐛 Bug Fix: Removed the status maintenance and handling from the GridCell as it breaks a lot of good practices, and causes UI update issues

--- lib/src/grid_cell.dart
+++ lib/src/grid_cell.dart
@@ -1,56 +1,36 @@
 import 'package:flutter/widgets.dart';
+import 'package:grid_board/src/grid_cell_controller.dart';
+
 import 'grid_cell_status.dart';
 
 typedef GridCellBuilder = Widget Function(GridCellStatus status);
 
 /// GridCell has multiple child widgets which defined with [gridCellChildMap]
 /// When the [GridCellStatus] changed, a transition effect applied to switch between childs.
-class GridCell extends StatefulWidget {
-  final GridCellStatus status;
-  GridCell({
-    Key? key,
+class GridCell extends StatelessWidget {
+  const GridCell({
     required this.gridCellChildMap,
-    this.status = GridCellStatus.initial,
+    required this.controller,
+    super.key,
     this.animationDuration = const Duration(milliseconds: 500),
-  }) : super(key: key);
+  });
 
   final Duration animationDuration;
-
-  /// Map of child widgets paired with one [GridCellStatus].
   final Map<GridCellStatus, Widget> gridCellChildMap;
-
-  @override
-  State<GridCell> createState() => myAppState;
-
-  final myAppState = _GridCellState();
-
-  void updateStatus(GridCellStatus status) {
-    myAppState.updateStatus(status);
-  }
-}
-
-class _GridCellState extends State<GridCell> {
-  late GridCellStatus currentStatus;
-  void updateStatus(GridCellStatus status) {
-    setState(() {
-      currentStatus = status;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    currentStatus = widget.status;
-  }
+  final GridCellController controller;
 
   @override
   Widget build(BuildContext context) {
-    //TODO: check is currentStatus defined
-    Widget currentChild = widget.gridCellChildMap[currentStatus]!;
-    return AnimatedSwitcher(
-      duration: widget.animationDuration,
-      child: currentChild,
-    );
+    return ListenableBuilder(
+        listenable: controller,
+        builder: (context, _) {
+          Widget currentChild =
+              gridCellChildMap[controller.status] ?? Container();
+          return AnimatedSwitcher(
+            duration: animationDuration,
+            child: currentChild,
+          );
+        });
   }
 }
 /*
