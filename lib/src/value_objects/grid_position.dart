91fcdd92460f385fd4d5258a1c4698389f6f4e15
NonymousMorlock <agamspaul@gmail.com>
2024-06-20 18:02:47 +0000

    Style: Formatting

--- lib/src/value_objects/grid_position.dart
+++ lib/src/value_objects/grid_position.dart
@@ -2,22 +2,22 @@ import 'package:grid_board/grid_board.dart';
 
 /// Specifies the row and column index of a grid cell
 class GridPosition {
+  const GridPosition({required this.columnIndex, required this.rowIndex});
+
+  factory GridPosition.fromIndex(GridSize size, int index) {
+    final row = index % size.rowCount;
+    final col = (index / size.rowCount).floor();
+    return GridPosition(columnIndex: col, rowIndex: row);
+  }
   final int columnIndex;
   final int rowIndex;
-  const GridPosition({required this.columnIndex, required this.rowIndex});
 
   @override
   String toString() {
-    return "GridPosition[columnIndex:$columnIndex, rowIndex:$rowIndex]";
+    return 'GridPosition[columnIndex:$columnIndex, rowIndex:$rowIndex]';
   }
 
   int toIndex(GridSize size) {
     return (size.colCount * rowIndex) + columnIndex;
-  }
-
-  factory GridPosition.fromIndex(GridSize size, int index) {
-    int row = index % size.rowCount;
-    int col = (index / size.rowCount).floor();
-    return GridPosition(columnIndex: col, rowIndex: row);
   }
 }
